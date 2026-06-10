da55f4e39b91f307edb4ed34c7d6e1e73280f975
vaalaav <34348558+vaalaav@users.noreply.github.com>
2026-06-10 14:42:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,44 +3,21 @@
 set -e
 
 REPO="vaalaav/myserverinfo"
-INSTALL_PATH="/usr/local/bin/myserverinfo"
-VERSION_PATH="/usr/local/share/myserverinfo.version"
-
-echo
-echo "===================================="
-echo "      Установка MyServerInfo"
-echo "===================================="
-echo
-
-if ! command -v curl >/dev/null 2>&1; then
-echo "Устанавливаю curl..."
-apt-get update
-apt-get install -y curl
-fi
 
 mkdir -p /usr/local/share
 
-echo "Скачивание последней версии..."
-
 curl -fsSL 
 https://raw.githubusercontent.com/${REPO}/main/myserverinfo 
--o ${INSTALL_PATH}
+-o /usr/local/bin/myserverinfo
 
-chmod +x ${INSTALL_PATH}
+chmod +x /usr/local/bin/myserverinfo
 
 curl -fsSL 
 https://raw.githubusercontent.com/${REPO}/main/VERSION 
--o ${VERSION_PATH}
-
-VERSION=$(cat ${VERSION_PATH})
+-o /usr/local/share/myserverinfo.version
 
 echo
-echo "===================================="
-echo "Установка завершена"
-echo "Версия: ${VERSION}"
-echo "===================================="
+echo "Установка завершена."
 echo
-echo "Для запуска выполните:"
-echo
+echo "Запуск:"
 echo "myserverinfo"
-echo
